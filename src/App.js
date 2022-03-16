f0043f7f26f4992c2b73874b246b740b8c14d0ae
Edvan Gomes <edvan.bgjunior@gmail.com>
2022-03-16 03:09:52 -0300

    Add a favicon and page name

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./pages/home";
 import Trades from './pages/trades';
 
 
 function App() {
+  const title = "PokeTrader";
+  useEffect(() => {
+    // This will run when the page first loads and whenever the title changes
+    document.title = title;
+  }, [title]);
+
+
   return (
     <div>
       <BrowserRouter>
